daad1c6511f25f525d5ad9b2e24c78adf2459ba0
dukelester <dukelester4@gmail.com>
2022-09-17 12:38:34 +0300

    finished user registration

--- users/authentication.py
+++ users/authentication.py
@@ -5,19 +5,27 @@ from .schemas import UserpasswordUpdate
 password_context = CryptContext(schemes=['sha256_crypt',], deprecated="auto")
 
 class Authenticate:
-    @staticmethod
-    def generate_salt() -> str:
-        return bcrypt.gensalt().decode()
+    # @staticmethod
+    # def generate_salt() -> str:
+    #     return bcrypt.gensalt().decode()
     
-    @staticmethod
-    def hashed_password(*, password: str, salt: str) -> str:
-        return password_context.hash(password, salt)
+    # @staticmethod
+    # def hashed_password(*, password: str, salt: str) -> str:
+    #     return password_context.hash(password, salt)
         
 
     def create_salt_hashed_password(self, *, plain_text_password: str) -> UserpasswordUpdate:
         salt = self.generate_salt()
-        hash_password = self.hashed_password(password = plain_text_password, salt=salt)
+        hash_password = self.hash_password(password = plain_text_password, salt=salt)
         return UserpasswordUpdate(hash_password=salt, password=hash_password)
+    
+    @staticmethod
+    def generate_salt() -> str:
+        return bcrypt.gensalt().decode()
+
+    @staticmethod
+    def hash_password(*, password: str, salt: str) -> str:
+        return password_context.hash(password + salt)
     
    
   
